6b9b6fa705f9fec5d407eaaf62ea1db492fbd67e
inklinsworld <95843810+inklinsworld@users.noreply.github.com>
2025-08-22 15:22:19 +0100

    added backing out of the pause menu

--- scripts/control.gd
+++ scripts/control.gd
@@ -10,4 +10,13 @@ func _on_button_2_pressed() -> void:
 
 
 func _on_h_slider_value_changed(value: float) -> void:
-	AudioServer.set_bus_volume_db(0, value)
+	AudioServer.set_bus_volume_db(0, value-50)
+
+
+func _on_check_button_toggled(toggled_on: bool) -> void:
+	AudioServer.set_bus_mute(0, toggled_on)
+
+
+func _on_button_pressed() -> void:
+	$base_menu.show()
+	$settings.hide()
