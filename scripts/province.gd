aec6afee8bcdbd2400a523c8c9d9954d5ec714c8
inklinsworld <inklinworld@gmail.com>
2025-08-31 00:08:14 +0100

    Naive Tooltip, refactored province.gd from scenes to scripts

--- scripts/province.gd
+++ scripts/province.gd
@@ -2,6 +2,8 @@ class_name province
 extends Area2D
 
 var is_in_area = false
+var tooltip = false
+@onready var line_2d: Line2D = $highlight_line
 
 
 func _on_mouse_entered() -> void:
@@ -13,6 +15,8 @@ func _on_mouse_exited() -> void:
 
 func _physics_process(delta):
 	if is_in_area:
-		pass #TODO: add test func
+		if Input.is_action_just_pressed("select"):
+			line_2d.show()
+			tooltip = true
 	else:
-		pass
+		line_2d.hide()
