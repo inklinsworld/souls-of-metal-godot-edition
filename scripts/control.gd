5d10404cd33158e29612a96a97270483fec7c477
inklinsworld <inklinworld@gmail.com>
2025-08-31 17:44:36 +0100

    added splash messages and naive provinces

--- scripts/control.gd
+++ scripts/control.gd
@@ -2,10 +2,13 @@ extends Control
 
 @onready var base_menu: VBoxContainer = $base_menu
 @onready var settings: VBoxContainer = $settings
+@onready var splash: Label = $splash
+var splash_text = ["we were a shitshow!", "pol's mum is fat!", "made primarily by Sockmit!", "Provinces by Inklin", "the founder abandoned us so we picked up the scraps!", "bow down to supreme leader!", "Jayjay is a legend", "also try HOI4!"]
 
 func _ready():
 	base_menu.show()
 	settings.hide()
+	splash.text = splash_text.pick_random()
 
 func _on_button_3_pressed() -> void:
 	get_tree().quit()
@@ -20,3 +23,7 @@ func _on_check_button_toggled(toggled_on: bool) -> void:
 func _on_button_pressed() -> void:
 	base_menu.show()
 	settings.hide()
+
+
+func _on_check_box_toggled(toggled_on: bool) -> void:
+	Globals.debug_mode = toggled_on #thanks pol for the advice
--- scripts/province.gd
+++ scripts/province.gd
@@ -4,10 +4,10 @@ extends Area2D
 var is_in_area = false
 var tooltip = false
 var city_level = 0
+var factory_count = 0
 @onready var line_2d: Line2D = $highlight_line
 #export to be removed when implemented properly, as the label will be the same for many provinces
 @export var label: Label
-
 
 func _on_mouse_entered() -> void:
 	is_in_area = true
@@ -25,12 +25,13 @@ func _process(delta):
 	else:
 		line_2d.hide()
 	if tooltip:
-		label.text = "Check out Neo Filch RPG!"
+		label.text = "city level: " + str(city_level) + "
+		factories: " + str(factory_count)
 		label.show()
 	else:
 		label.hide()
 		label.text = ""
 
 func _physics_process(delta):
-	pass
-	#TODO: add city code
+	for city in range(0, city_level):
+		factory_count += 2
